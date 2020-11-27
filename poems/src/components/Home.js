8c6632359d884d62d2329684da51b2ecd8d83ded
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-11-27 18:59:39 -0500

    Files made for poem mutation.

--- poems/src/components/Home.js
+++ poems/src/components/Home.js
@@ -10,7 +10,6 @@ const Home = (props) => {
         props.getWheatleyData() 
         props.getHammonData() 
         props.getDunbarData()
-        console.log(props)
     }, []) 
     
     return (
