bad8f9405b44d9690d4059ee8af3c016c764f60f
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-11-30 10:13:23 -0500

    get_functions.py fully functioning and using classes

--- classes.py
+++ classes.py
@@ -1,11 +1,11 @@
 class Poem(object): 
-    def __init__(self, title, lines, line_count):
+    def __init__(self, title, lines, line_count, words=[], fitness=0, probability=0):
         self.title = title
         self.lines = lines 
         self.line_count = line_count
-        self.fitness = 0
-        self.probability = 0
-        self.total = 0 
+        self.words = words 
+        self.fitness = fitness
+        self.probability = probability
 
     def __str__(self):
         return f"Title: {self.title}\n Fitness: {self.fitness}\n"
@@ -14,7 +14,22 @@ class Poem(object):
         self.probability = self.fitness / total
 
 
-class Line(object):
-    def __init__(self, words, structure):
-        self.words = words 
-        self.structure = {}
+class Word(object):
+    def __init__(self, word, part_of_speach, sequence_list, frequency=0):
+        self.word = word
+        self.part_of_speach = part_of_speach
+        self.sequence_list = sequence_list
+        self.frequency = len(sequence_list)
+
+    def __str__(self):
+        return f"Word: {self.word}\n Type: {self.part_of_speach}\n"
+
+
+class Sequence(object): 
+    def __init__(self, seq_string, count=1): 
+        self.seq_string = seq_string
+        self.count = count 
+
+
+
+    
--- get_functions.py
+++ get_functions.py
@@ -1,14 +1,15 @@
 import requests 
-
+from classes import Poem, Word, Sequence
+ 
 # Create global constant for all poets' poem urls retrieved from poetrydb.org API 
-POETS = {
-    # "dunbar_url": "https://poetrydb.org/author/Paul%20Laurence%20Dunbar",
+POET_URLS = {
     "hammon_url": "https://poetrydb.org/author/Jupiter%20Hammon",
-    # "wheatley_url": "https://poetrydb.org/author/Phillis%20Wheatley",
 }
 
 def get_poems(urls): 
-    # Stores all POEMS within the training set 
+    '''Use POET_URLS object as urls. Loop through the object and 
+    retrieve the poems. Returns an list of poems.'''
+
     poems = [] 
 
     for key in urls: 
@@ -17,45 +18,125 @@ def get_poems(urls):
 
         # Check if 200 response recieved 
         if response.ok:
-            # Add JSON response data array to the POEMS array 
+            # Add JSON response data list to the POEMS list 
             poems.extend(response.json())
 
     return poems 
 
-POEMS = get_poems(POETS)
+def get_words_and_sequences(poems):
+    '''Retrieves every word from each poem in the poems list
+    and returns them in a dictionary called all_word_sequences
+    (all words and sequences). Also creates, Poem objects
+    for each poem in the poems list.'''
 
-def get_word_sequence(poems_array):
-    word_segments = {}
+    all_word_sequences = {}
+    final_poems_list = [] 
 
-    for poem in poems_array:
+    for poem in poems:
+        poem_title = poem["title"]
+        poem_lines = poem["lines"]
+        poem_line_count = poem["linecount"]
+        poem_words_object = dict()
+
         for line in poem["lines"]:
-            line_array = line.split()
-            num_words = len(line_array)
+            line_list = line.split()
+            num_words = len(line_list)
 
-            if num_words == 1:
-                sequence = line_array[0] + "  "
-                word_segments[current_word] = sequence
+            if (num_words == 1):
+                # line only has 1 one word
+                word = line_list[0]  
+                sequence = word + "\n"
+                
+                all_word_sequences[word] = sequence
+                poem_words_object[word] = sequence
                 continue
             elif (num_words == 0):
-                word_segments["emptyspace"] = " "
-                continue 
-
-            current_word = line_array[0]
-
-            if (word_segments.get(current_word) == None):
-                    word_segments[current_word] = [] 
-
-            print(word_segments[current_word], line_array[0:2])   
-
-            while len(line_array) > 2:
-                sequence = current_word + " " + line_array[1]
-                word_segments[current_word] = word_segments[current_word] + [sequence]
-                
-                del line_array[0]
-
+                # line is an empty string 
+                word = "newline"
+                word_part_of_speach = None 
+                sequence = "\n"
     
-    return word_segments
+                all_word_sequences[word] = sequence
+                poem_words_object[word] = sequence
+                continue 
+            
+            word = line_list[0] 
 
-get_word_sequence(POEMS)  
+            # Check if word exists in both dictionaries 
+            if (all_word_sequences.get(word) == None):
+                all_word_sequences[word] = [] 
+                poem_words_object[word] = []
+            elif (poem_words_object.get(word) == None):
+                poem_words_object[word] = [] 
+
+            # Create sequences of len=2 by concatenating word string
+            # with every word in the line 
+            while len(line_list) > 2:
+                sequence = word + " " + line_list[1]
+
+                all_word_sequences[word] = all_word_sequences[word] + [sequence]
+                poem_words_object[word] = poem_words_object[word]  + [sequence]
+                
+                del line_list[0]
+        
+        # Get Word objects for current poem using poem_words_object 
+        poem_words_list = get_word_objects(poem_words_object)
+
+        # Create a new poem 
+        new_poem = Poem(poem_title, poem_lines, poem_line_count, poem_words_list)
+
+        # Add the poem to the final_poems_list 
+        final_poems_list.append(new_poem)
+    
+    return all_word_sequences, final_poems_list
 
 
+def get_word_objects(word_sequences):
+    
+    words = [] 
+
+    for key in word_sequences: 
+        word = key 
+        part_of_speach = get_part_of_speach(word)
+        sequence_list = [] 
+        sequences = word_sequences[word]
+
+        for sequence in sequences:
+            count = word_sequences[word].count(word)
+            new_sequence = Sequence(sequence, count)
+            sequence_list.append(new_sequence)
+            
+            if (count > 1):
+                # Remove all occurances from sequences 
+                while sequence.count(word) != 0:
+                    sequences.remove(word)
+        
+        new_word = Word(word, part_of_speach, sequence_list)
+        words.append(new_word)
+    
+    return words 
+
+
+def get_part_of_speach(word):
+    
+    lowercase_word = word.lower().replace(",.!", "")
+    api_key = "aeb2df49-a687-4538-8d72-581581a973ad"
+    url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + lowercase_word + "?key=" + api_key
+
+    response = requests.request('GET', url)
+
+    # Check if 200 response recieved 
+    if response.ok:
+        if (type(response.json()[0]) == dict):
+            part_of_speach = response.json()[0]["fl"]
+        elif (type(response.json()[0]) == str):
+            # newline 
+            part_of_speach = None
+
+    return part_of_speach 
+
+
+a = get_poems(POET_URLS)
+b, c = get_words_and_sequences(a)
+
+print("This is c\n", c)
